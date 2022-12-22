f340ecbb8ed7e9bfb1f303f186c2c9d27b5a7e61
John Leamy <jwleamy@gmail.com>
2022-12-21 18:07:49 -0800

    scroll animations

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -5,3 +5,18 @@ function openNav() {
 function closeNav() {
     $("#mynavbar").css('width', '100px')
   }
+
+var myarray = 'hippo'
+
+
+//----------- Scroll Effects With JavaScript functions --------------//
+$(window).scroll(function () {
+        $('.hidden').each(function (i) {
+            var bottom_of_object = $(this).offset().top + ($(this).outerHeight() * 0.3);
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            if (bottom_of_window > bottom_of_object) {
+                $(this).addClass('show')
+            }
+        })});
+
+$('.block').smoove({offset: '40%'});
